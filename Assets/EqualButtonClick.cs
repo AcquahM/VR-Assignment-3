dcbcc5cc26bdc2e664ae1afbc2b3d033efd83e09
Constantino <51053440+LeonKang130@users.noreply.github.com>
2022-12-03 21:59:34 +0800

    fix multiple operators in one evaluation

--- Assets/EqualButtonClick.cs
+++ Assets/EqualButtonClick.cs
@@ -20,21 +20,25 @@ public class EqualButtonClick : MonoBehaviour
     {
 
     }
-
+    
     public void Click()
     {
-        string text = displayText.text;
-        int num1 = 0, num2 = 0, op = 0;
-        for (int i = 0; i < text.Length; i++)
+        var text = displayText.text;
+        int acc = 0, delta = 0, sign = 1;
+        for(var i = 0; i < text.Length; i++)
         {
             if (text[i] != '+' && text[i] != '-')
             {
-                if (op == 0) num1 = num1 * 10 + (text[i] - 48);
-                else num2 = num2 * 10 + (text[i] - 48);
+                delta = delta * 10 + (text[i] - 48);
             }
-            else if (text[i] == '+') op = 1;
-            else if (text[i] == '-') op = 2;
+            else
+            {
+                acc += sign * delta;
+                delta = 0;
+                sign = text[i] == '+' ? 1 : -1;
+            }
         }
-        displayText.text += buttonText.text + ((op == 1) ? (num1 + num2) : (num1 - num2)).ToString();
+        acc += sign * delta;
+        displayText.text = $"{text}={acc.ToString()}";
     }
 }
